d3f61c5ea64587476043583ea36b330be1dc5e5b
shimoncohen <shimoncohen655@gmail.com>
2020-07-19 19:00:38 +0300

    Added requests to server: * search resource by part of name * search project by part of name

--- src/General/Requests.js
+++ src/General/Requests.js
@@ -3,6 +3,20 @@ import Config from './Config';
 
 export async function GetProjects(path){
     const serachPath = path ? '?path='+path : '';
-    const projects = await Axios.get(Config.urlGetProjects + serachPath); 
+    const projects = await Axios.get(Config.urlGetProjects + serachPath);
     return projects.data;
+}
+
+async function Search(rootPath, name) {
+    const query = '?name=' + name;
+    const results = await Axios.get(rootPath + query);
+    return results.data;
+}
+
+export async function SearchProjects(name) {
+    return await Search(Config.urlSearchProjects, name);
+}
+
+export async function SearchResources(name) {
+    return await Search(Config.urlSearchResources, name);
 }
